c6f70d4b039fff736e3c783a1a027bb6d1182c51
arun__kumar <49428577+kumarisback@users.noreply.github.com>
2022-07-03 13:39:15 +0530

    Create Merge intervals.java

---
+++ Merge intervals.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+     List<int[]> l=new ArrayList<>();
+        
+        if(intervals.length==0 || intervals==null) 
+            return l.toArray( new int[0][]);
+        
+        Arrays.sort(intervals,(a,b)->  a[0]-b[0]);
+        
+        int start=intervals[0][0];
+        int end= intervals[0][1];
+        
+        for(int[] i: intervals){
+            if(i[0]<=end)
+                end=Math.max(i[1],end);
+            else{
+                l.add(new int[]{start,end});
+                start=i[0];
+                end=i[1];
+            }
+        }
+        l.add(new int[]{start,end});
+        return l.toArray(new int[0][]);
+        
+    }
+}
