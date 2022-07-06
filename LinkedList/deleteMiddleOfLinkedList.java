3150fd2f79dbb5e0791c6e25de5fcb0ecafe092e
arun__kumar <49428577+kumarisback@users.noreply.github.com>
2022-07-06 22:30:14 +0530

    linkedlist

---
+++ LinkedList/deleteMiddleOfLinkedList.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        
+        int len=0;
+        ListNode temp=head;
+        while(temp!=null){
+            len++;
+            temp=temp.next;
+        }
+        temp=head;
+        for(int i=0;i<(len/2);i++){
+            temp=temp.next;
+        }
+        return temp;
+    }
+}
